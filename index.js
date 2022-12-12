51acd13cfec16935b92cee0129620b51fb8b5bb6
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-12-12 16:54:02 +0600

    database connect

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const cors = require(cors);
+require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
@@ -14,40 +15,15 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
 async function run() {
-    try {
-client.connect();
-
-        const collection = client.db("test").collection("devices");
-      // Query for a movie that has the title 'The Room'
-      const query = { title: "The Room" };
-      const options = {
-        // sort matched documents in descending order by rating
-        sort: { "imdb.rating": -1 },
-        // Include only the `title` and `imdb` fields in the returned document
-        projection: { _id: 0, title: 1, imdb: 1 },
-      };
-      const movie = await movies.findOne(query, options);
-      // since this method returns the matched document, not a cursor, print it directly
-      console.log(movie);
-    } finally {
-      await client.close();
-    }
+  try {
+    client.connect();
+    const servicesCollection = client.db("clean-co").collection("services");
+    console.log("db connect");
+  } finally {
   }
-  run().catch(console.dir);
-
-
-
-
-
-
-
-
-
-
-
-
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
